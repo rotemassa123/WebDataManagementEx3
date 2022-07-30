ac628dd841206d5f831760cc102693553ac9723e
Rotem Assa <rotem_a@optimove.com>
2022-07-30 13:51:06 +0300

    now tokenizing words properly fixed bugs

--- InvertedIndexDictionary.py
+++ InvertedIndexDictionary.py
@@ -1,34 +1,47 @@
+import math
 import xml.etree.ElementTree as ET
+
+import nltk
+from nltk import RegexpTokenizer
 from nltk.corpus import stopwords
 import os
 from collections import Counter
 import json
+from nltk.stem import PorterStemmer
 
-TERM_FREQUENCY = "term_frequency"
-DOCUMENT_FREQUENCY = "document_frequency"
-DOCUMENT_LENGTHS = "document_lengths"
+OUTPUT_FILE = "output.json"
+
 
 class InvertedIndexDictionary:
 
     def __init__(self, path_to_xml_dir):
         self.path_to_xml_dir = path_to_xml_dir
-        self.term_frequency = {}
-        self.document_frequency = {}
-        self.doc_len_dict = {}
+        self.dict = {}
+        try:
+            stop_words = set(stopwords.words("english"))
+        except:
+            nltk.download('stopwords')
+            stop_words = set(stopwords.words("english"))
+
         self.words_to_stop = set(stopwords.words('english'))
         self.words_to_stop.add("\n")
         self.count_of_docs = 0
 
     def build_inverted_index(self):
         files_list = [file for file in os.listdir(self.path_to_xml_dir) if file.endswith(".xml")]
+        term_frequency = {}
+        log_inverted_document_frequency = {}
+        len_by_doc_name = {}
         for file in files_list:
             count_of_docs_in_file, counter_dict_for_file, doc_len_dict_for_file = self.get_inverted_index_of_file(file)
             self.count_of_docs += count_of_docs_in_file
-            self.merge_two_freq_dicts(self.term_frequency, counter_dict_for_file)
-            self.doc_len_dict.update(doc_len_dict_for_file)
+            self.merge_two_freq_dicts(term_frequency, counter_dict_for_file)
+            len_by_doc_name.update(doc_len_dict_for_file)
 
-        for word in self.term_frequency:
-            self.document_frequency[word] = len(self.term_frequency[word]) / self.count_of_docs
+        for word in term_frequency:
+            log_inverted_document_frequency[word] = math.log2(self.count_of_docs / len(term_frequency[word]))
+
+        self.dict = {"TF": term_frequency, "len_by_doc_name": len_by_doc_name, "IDF": log_inverted_document_frequency}
 
     # returns a dictionary containing word and their frequencies in file (takes words from title and
     def get_inverted_index_of_file(self, file):
@@ -37,25 +50,44 @@ class InvertedIndexDictionary:
         counter_dict_for_file = {}
         doc_len_dict_for_file = {}
         count_of_docs_in_file = len(root.findall("RECORD"))
+
         for record in root:
-            filtered_but_not_tokenized_words = []
             counter_dict_for_doc = {}
             paper_num = record.find("PAPERNUM")
-            title = record.find("TITLE")
-            body = record.find("ABSTRACT")
+            len_of_doc, words = self.get_tokenized_words_from_record(record)
+            doc_len_dict_for_file[paper_num.text] = len_of_doc
+            term_freq_dict_for_doc = Counter(words)
 
-            if title is not None and body is not None:
-                words = " ".join([body.text, title.text]).replace("\n", ' ').split(' ')
-                filtered_but_not_tokenized_words = [word for word in words if not word.lower() in self.words_to_stop]
-                doc_len_dict_for_file[paper_num.text] = len(words)
-
-            term_freq_dict_for_doc = Counter(filtered_but_not_tokenized_words)
             for word, count in term_freq_dict_for_doc.items():
                 counter_dict_for_doc[word] = (paper_num.text, count)
-
             self.merge_two_freq_dicts(counter_dict_for_file, counter_dict_for_doc)
 
         return count_of_docs_in_file, counter_dict_for_file, doc_len_dict_for_file
+
+    def get_tokenized_words_from_record(self, record):
+        text = ""
+        title = record.find("TITLE")
+        abstract = record.find("ABSTRACT")
+        extract = record.find("EXTRACT")
+
+        if title is not None:
+            text += str(title.text)
+        if abstract is not None:
+            text = " ".join([text, str(abstract.text)])
+        if extract is not None:
+            text = " ".join([text, str(extract.text)])
+
+        return self.get_tokenized_filtered_and_stemmed_words(text)
+
+    def get_tokenized_filtered_and_stemmed_words(self, text):
+        tokenizer = RegexpTokenizer(r'\w+')
+        porter_stemmer = PorterStemmer()
+
+        words = tokenizer.tokenize(text.lower())
+        filtered_words = [word for word in words if not word in self.words_to_stop]
+
+        return len(words), filtered_words
+
 
     @staticmethod
     def merge_two_freq_dicts(dict1, dict2):
@@ -65,4 +97,12 @@ class InvertedIndexDictionary:
             else:
                 dict1[word] = dict2[word]
 
+    def save_data_to_files(self, path=""):
+        with open(path + OUTPUT_FILE, "w") as outfile:
+            json.dump(self.dict, outfile)
 
+    def load_data_from_files(self, path=""):
+        with open(path + OUTPUT_FILE) as json_file:
+            dict = json.load(json_file)
+
+        return dict
--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from collections import Counter
 
 from InvertedIndexDictionary import InvertedIndexDictionary
 
+def get_dict_diffs(dict1, dict2):
+    set1 = set(dict1.items())
+    set2 = set(dict2.items())
+    print(set1 ^ set2)
+
 if __name__ == '__main__':
-    dict = InvertedIndexDictionary("xml_input_files")
-    dict.build_inverted_index()
-    print("yes")
+    inverted_index = InvertedIndexDictionary("xml_input_files")
+    inverted_index.build_inverted_index()
+    inverted_index.save_data_to_files("output_files/")
+    dict = inverted_index.load_data_from_files("output_files/")
